e87e3ef2771579eaa21ff939cee25f44b0b64fe3
Paula Pinochet <paula.pinochet.p@gmail.com>
2015-07-11 11:46:13 -0300

    added underscore.js added requirements in application.js and its working :D

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,91 @@
 class StoresController < InheritedResources::Base
 
+  before_action :set_store, only: [:show, :edit, :update, :destroy]
+
+  # GET /Stores
+  # GET /Stores.json
+  def index
+    if params[:search].present?
+        @stores = Store.near(params[:search], 50, :order => :distance)
+    else
+        @stores = Store.all
+    end
+  end
+
+  # GET /Stores/1
+  # GET /Stores/1.json
+  def show
+      @store = Store.find(params[:id])
+      @result = request.location
+
+      @stores = Store.near([@store.latitude, @store.longitude], 5)
+      @hash = Gmaps4rails.build_markers(@stores) do |stores, marker|
+      marker.lat stores.latitude
+      marker.lng stores.longitude
+    end
+  end
+
+  # GET /Stores/new
+  def new
+    @store = Store.new
+  end
+
+  # GET /Stores/1/edit
+  def edit
+  end
+
+  # POST /Stores
+  # POST /Stores.json
+  def create
+    @store = Store.new(store_params)
+
+    respond_to do |format|
+      if @store.save
+        format.html { redirect_to @store, notice: 'Store was successfully created.' }
+        format.json { render :show, status: :created, store: @store }
+      else
+        format.html { render :new }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /Stores/1
+  # PATCH/PUT /Stores/1.json
+  def update
+    respond_to do |format|
+      if @store.update(store_params)
+        format.html { redirect_to @store, notice: 'Store was successfully updated.' }
+        format.json { render :show, status: :ok, store: @store }
+      else
+        format.html { render :edit }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /Stores/1
+  # DELETE /Stores/1.json
+  def destroy
+    @store.destroy
+    respond_to do |format|
+      format.html { redirect_to stores_url, notice: 'Store was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_store
+      @store = Store.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def store_params
+      params.require(:store).permit(:address, :latitude, :longitude)
+    end
+
+
   private
 
     def store_params
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,5 @@
 class Store < ActiveRecord::Base
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
   belongs_to :user
 end
