b387845525f8132b041ca498d721d01353c4e29f
Paula Pinochet <paula.pinochet.p@gmail.com>
2015-07-11 14:54:53 -0300

    Added location to user in index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :stores
-  devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
